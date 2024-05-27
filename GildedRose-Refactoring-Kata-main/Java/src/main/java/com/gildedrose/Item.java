4844e26986d7af2360f02eff33a93be8806cf4ce
flo629 <144320013+flo629@users.noreply.github.com>
2024-05-27 23:55:42 +0200

    variablen herausgehoben

--- GildedRose-Refactoring-Kata-main/Java/src/main/java/com/gildedrose/Item.java
+++ GildedRose-Refactoring-Kata-main/Java/src/main/java/com/gildedrose/Item.java
@@ -1,69 +1,49 @@
-package com.gildedrose;
+public void updateItem() {
+    boolean isAgedBrieOrBackstagePass = name.equals("Aged Brie") || name.equals("Backstage passes to a TAFKAL80ETC concert");
+    boolean isBackstagePass = name.equals("Backstage passes to a TAFKAL80ETC concert");
+    boolean isSulfuras = name.equals("Sulfuras, Hand of Ragnaros");
 
-public class Item {
+    if (!isAgedBrieOrBackstagePass) {
+        if (quality > 0 && !isSulfuras) {
+            quality--;
+        }
+    } else {
+        if (quality < 50) {
+            quality++;
 
-    public String name;
-    public int sellIn;
-    public int quality;
+            if (isBackstagePass) {
+                if (sellIn < 11) {
+                    if (quality < 50) {
+                        quality++;
+                    }
+                }
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
+                if (sellIn < 6) {
+                    if (quality < 50) {
+                        quality++;
+                    }
+                }
+            }
+        }
     }
 
-    public void updateItem() {
-        if (!name.equals("Aged Brie") && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (quality > 0) {
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                    quality--;
-                }
+    if (!isSulfuras) {
+        sellIn--;
+    }
+
+    if (sellIn < 0) {
+        if (!isAgedBrieOrBackstagePass) {
+            if (quality > 0 && !isSulfuras) {
+                quality--;
             }
         } else {
-            if (quality < 50) {
-                quality++;
-
-                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality++;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality++;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-            sellIn--;
-        }
-
-        if (sellIn < 0) {
-            if (!name.equals("Aged Brie")) {
-                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            quality--;
-                        }
-                    }
-                } else {
-                    quality = 0;
-                }
+            if (isBackstagePass) {
+                quality = 0;
             } else {
                 if (quality < 50) {
                     quality++;
                 }
             }
         }
-    }
-
-    @Override
-    public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
